961e0dcdcef30f789d2e46482b2ce0ad6cf4686b
Adam Goode <agoode@andrew.cmu.edu>
2008-01-16 09:50:47 -0500

    Reformat

--- src/edu/cmu/cs/diamond/opendiamond/Searchlet.java
+++ src/edu/cmu/cs/diamond/opendiamond/Searchlet.java
@@ -19,17 +19,19 @@ import java.util.List;
 
 public class Searchlet {
     final private List<Filter> filters = new ArrayList<Filter>();
+
     private String[] dependencies;
-    
+
     public void addFilter(Filter f) {
         filters.add(f);
     }
-    
+
     public void setApplicationDependencies(String dependencies[]) {
         this.dependencies = new String[dependencies.length];
-        System.arraycopy(dependencies, 0, this.dependencies, 0, dependencies.length);
+        System.arraycopy(dependencies, 0, this.dependencies, 0,
+                dependencies.length);
     }
-    
+
     File createFilterSpecFile() throws IOException {
         File out = File.createTempFile("filterspec", ".txt");
         out.deleteOnExit();
@@ -39,7 +41,7 @@ public class Searchlet {
         w.close();
         return out;
     }
-    
+
     File[] createFilterFiles() throws IOException {
         File result[] = new File[filters.size()];
 
@@ -47,24 +49,25 @@ public class Searchlet {
         for (Filter f : filters) {
             File file = File.createTempFile("filter", ".bin");
             file.deleteOnExit();
-            
-            DataOutputStream out = new DataOutputStream(new FileOutputStream(file));
+
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(
+                    file));
             out.write(f.getFilterCode().getBytes());
             out.close();
-            
+
             result[i++] = file;
         }
-        
+
         return result;
     }
-    
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (Filter f : filters) {
             sb.append(f.toString());
         }
-        
+
         if (dependencies != null) {
             sb.append("FILTER APPLICATION\n");
             for (String d : dependencies) {
