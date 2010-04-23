7cb07a29cd0b25af3cd07b7abb019c98619d3a54
Adam Goode <agoode@andrew.cmu.edu>
2010-04-23 10:27:19 -0400

    Allow for disabling logging

--- src/edu/cmu/cs/diamond/opendiamond/LoggingFramework.java
+++ src/edu/cmu/cs/diamond/opendiamond/LoggingFramework.java
@@ -6,6 +6,41 @@ import java.util.Set;
 
 abstract class LoggingFramework {
 
+    private static final LoggingFramework NULL_LOGGER = new LoggingFramework() {
+        @Override
+        public void updateStatistics(Map<String, ServerStatistics> result) {
+        }
+
+        @Override
+        public void stoppedSearch(Throwable cause) {
+        }
+
+        @Override
+        public void startedSearch() {
+        }
+
+        @Override
+        public void shutdown(Throwable cause) {
+        }
+
+        @Override
+        public void saveSessionVariables(Map<String, Double> sessionVariables) {
+        }
+
+        @Override
+        public void saveSearchFactory(SearchFactory searchFactory,
+                Set<String> desiredAttributes) {
+        }
+
+        @Override
+        public void saveGetNewResult(Result result) {
+        }
+
+        @Override
+        public void logNoMoreResults() {
+        }
+    };
+
     public abstract void saveSearchFactory(SearchFactory searchFactory,
             Set<String> desiredAttributes) throws IOException;
 
@@ -24,7 +59,16 @@ abstract class LoggingFramework {
 
     public abstract void shutdown(Throwable cause);
 
-    public static LoggingFramework createLoggingFramework(String message) throws IOException {
-        return new XMLLogger(message);
+    public static LoggingFramework createLoggingFramework(String message)
+            throws IOException {
+        boolean enabled = Boolean.parseBoolean(System.getProperty(
+                "edu.cmu.cs.diamond.opendiamond.loggingframework.enabled",
+                "true"));
+
+        if (enabled) {
+            return new XMLLogger(message);
+        } else {
+            return NULL_LOGGER;
+        }
     }
 }
