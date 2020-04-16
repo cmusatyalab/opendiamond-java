ee14783d43012ce22ec9ce4863dedff7e39db891
Haithem Turki <hturki@cs.cmu.edu>
2020-04-16 15:00:38 -0400

    disable logging by default

--- src/main/java/edu/cmu/cs/diamond/opendiamond/XMLLogger.java
+++ src/main/java/edu/cmu/cs/diamond/opendiamond/XMLLogger.java
@@ -99,7 +99,7 @@ class XMLLogger extends LoggingFramework {
             if (logMessage != null) {
                 this.javaLogger.log(Level.FINEST,
                         "Initializing new LoggingFramework for a new search.",
-                        logMessage);          	
+                        logMessage);
             } else {
                 this.javaLogger.log(Level.FINEST,
                         "Initializing new LoggingFramework for a new search.");
@@ -123,205 +123,181 @@ class XMLLogger extends LoggingFramework {
     }
 
     private void saveFilters(List<Filter> filters) throws IOException {
-    	if (filters != null) {
-	        for (Filter f : filters) {
-	            javaLogger.log(Level.FINEST, "Saving filter.", saveFilter(f));
-	        }
-    	}
+        if (filters != null) {
+            for (Filter f : filters) {
+                javaLogger.log(Level.FINEST, "Saving filter.", saveFilter(f));
+            }
+        }
     }
 
     private String[] saveFilter(Filter filter) throws IOException {
-    	String[] returnArray;
-    	ArrayList<String> fileNames = new ArrayList<String>();
-    	if (filter != null) {
-    		FileOutputStream fileOut1, fileOut2, fileOut3, fileOut4, fileOut5;
-    		String fileName1, fileName2, fileName3, fileName4, fileName5;
-    		fileName1 = Util.joinPaths(searchDir, "filter_" + filterCounter);
-    		fileName2 = Util.joinPaths(searchDir, "filtercode_" + filterCounter);
-    		fileName3 = Util.joinPaths(searchDir, "dependencies_" + filterCounter);
-    		fileName4 = Util.joinPaths(searchDir, "arguments_" + filterCounter);
-    		fileName5 = Util.joinPaths(searchDir, "blob_" + filterCounter);
-    		fileNames.add(fileName1);
-    		fileNames.add(fileName2);
-    		fileNames.add(fileName3);
-    		fileNames.add(fileName4);
-    		fileNames.add(fileName5);
-    		File f1 = new File(fileName1);
-    		File f2 = new File(fileName2);
-    		File f3 = new File(fileName3);
-    		File f4 = new File(fileName4);
-    		File f5 = new File(fileName5);
+        String[] returnArray;
+        ArrayList<String> fileNames = new ArrayList<String>();
+        if (filter != null) {
+            String fileName1, fileName2, fileName3, fileName4, fileName5;
+            fileName1 = Util.joinPaths(searchDir, "filter_" + filterCounter);
+            fileName2 = Util.joinPaths(searchDir, "filtercode_" + filterCounter);
+            fileName3 = Util.joinPaths(searchDir, "dependencies_" + filterCounter);
+            fileName4 = Util.joinPaths(searchDir, "arguments_" + filterCounter);
+            fileName5 = Util.joinPaths(searchDir, "blob_" + filterCounter);
+            fileNames.add(fileName1);
+            fileNames.add(fileName2);
+            fileNames.add(fileName3);
+            fileNames.add(fileName4);
+            fileNames.add(fileName5);
+            File f1 = new File(fileName1);
+            File f2 = new File(fileName2);
+            File f3 = new File(fileName3);
+            File f4 = new File(fileName4);
+            File f5 = new File(fileName5);
 
-    		fileOut1 = new FileOutputStream(f1);
-    		fileOut2 = new FileOutputStream(f2);
-    		fileOut3 = new FileOutputStream(f3);
-    		fileOut4 = new FileOutputStream(f4);
-    		fileOut5 = new FileOutputStream(f5);
-    		try {
-    			if (filter.getName() != null) {
-    				fileOut1
-    				.write((Base64.encodeBytes(filter.getName().getBytes()) + "\n")
-    						.getBytes());
-    			} else {
-    				fileOut1
-    				.write("--\n".getBytes());
-    			}
-    			
-			fileOut1.write((Double.toString(filter.getMinScore()) + "\n")
-					.getBytes());
+            try (FileOutputStream fileOut1 = new FileOutputStream(f1);
+                 FileOutputStream fileOut2 = new FileOutputStream(f2);
+                 FileOutputStream fileOut3 = new FileOutputStream(f3);
+                 FileOutputStream fileOut4 = new FileOutputStream(f4);
+                 FileOutputStream fileOut5 = new FileOutputStream(f5);) {
+                if (filter.getName() != null) {
+                    fileOut1
+                            .write((Base64.encodeBytes(filter.getName().getBytes()) + "\n")
+                                    .getBytes());
+                } else {
+                    fileOut1
+                            .write("--\n".getBytes());
+                }
 
-			fileOut1.write((Double.toString(filter.getMaxScore()) + "\n")
-    					.getBytes());
-    			
-    			if (filter.getFilterCode() != null)
-    				fileOut2.write(filter.getFilterCode().getBytes());
+                fileOut1.write((Double.toString(filter.getMinScore()) + "\n")
+                        .getBytes());
 
-    			if (filter.getDependencies() != null) {
-    				for (String s : filter.getDependencies()) {
-    					if (s != null) {
-    						fileOut3.write((Base64.encodeBytes(s.getBytes()) + "\n")
-    							.getBytes());
-    					}  else {
-    	    				fileOut3
-    	    				.write("--\n".getBytes());
-    	    			}
-    				}
-    			}
+                fileOut1.write((Double.toString(filter.getMaxScore()) + "\n")
+                        .getBytes());
 
-    			if (filter.getArguments() != null) {
-    				for (String s : filter.getArguments()) {
-    					if (s != null) {
-    						fileOut4.write((Base64.encodeBytes(s.getBytes()) + "\n")
-    							.getBytes());
-    					} else {
-    	    				fileOut4
-    	    				.write("--\n".getBytes());
-    	    			}
-    				}
-    			}
+                if (filter.getFilterCode() != null)
+                    fileOut2.write(filter.getFilterCode().getBytes());
 
-    			if (filter.getBlob() != null)
-    				fileOut5.write(filter.getBlob());
-    		} finally {
-    			try {
-    				fileOut1.close();
-    			} catch (IOException ignore) {
-    			}
-    			try {
-    				fileOut2.close();
-    			} catch (IOException ignore) {
-    			}
-    			try {
-    				fileOut3.close();
-    			} catch (IOException ignore) {
-    			}
-    			try {
-    				fileOut4.close();
-    			} catch (IOException ignore) {
-    			}
-    			try {
-    				fileOut5.close();
-    			} catch (IOException ignore) {
-    			}
-    		}
+                if (filter.getDependencies() != null) {
+                    for (String s : filter.getDependencies()) {
+                        if (s != null) {
+                            fileOut3.write((Base64.encodeBytes(s.getBytes()) + "\n")
+                                    .getBytes());
+                        } else {
+                            fileOut3
+                                    .write("--\n".getBytes());
+                        }
+                    }
+                }
 
-			returnArray = fileNames.toArray(new String[fileNames.size()]);
-    		// delete files that reference null data...
-    		if (filter.getFilterCode() == null) {
-    			f2.delete();
-    			fileNames.remove(fileName2);
-    			returnArray = fileNames.toArray(new String[fileNames.size()]);
-    		}
-    		if (filter.getDependencies() == null) {
-    			f3.delete();
-				fileNames.remove(fileName3);
-				returnArray = fileNames.toArray(new String[fileNames.size()]);
-    		}
-    		if (filter.getArguments() == null) {
-    			f4.delete();
-				fileNames.remove(fileName4);
-				returnArray = fileNames.toArray(new String[fileNames.size()]);
-    		}
-    		if (filter.getBlob() == null) {
-    			f5.delete();
-				fileNames.remove(fileName5);
-				returnArray = fileNames.toArray(new String[fileNames.size()]);
-    		}
+                if (filter.getArguments() != null) {
+                    for (String s : filter.getArguments()) {
+                        if (s != null) {
+                            fileOut4.write((Base64.encodeBytes(s.getBytes()) + "\n")
+                                    .getBytes());
+                        } else {
+                            fileOut4
+                                    .write("--\n".getBytes());
+                        }
+                    }
+                }
 
-    		filterCounter++;
-    		return returnArray;
-    	}
-    	return null;
+                if (filter.getBlob() != null)
+                    fileOut5.write(filter.getBlob());
+            }
+
+            returnArray = fileNames.toArray(new String[fileNames.size()]);
+            // delete files that reference null data...
+            if (filter.getFilterCode() == null) {
+                f2.delete();
+                fileNames.remove(fileName2);
+                returnArray = fileNames.toArray(new String[fileNames.size()]);
+            }
+            if (filter.getDependencies() == null) {
+                f3.delete();
+                fileNames.remove(fileName3);
+                returnArray = fileNames.toArray(new String[fileNames.size()]);
+            }
+            if (filter.getArguments() == null) {
+                f4.delete();
+                fileNames.remove(fileName4);
+                returnArray = fileNames.toArray(new String[fileNames.size()]);
+            }
+            if (filter.getBlob() == null) {
+                f5.delete();
+                fileNames.remove(fileName5);
+                returnArray = fileNames.toArray(new String[fileNames.size()]);
+            }
+
+            filterCounter++;
+            return returnArray;
+        }
+        return null;
     }
 
     private void saveAttributes(Set<String> desiredAttributes)
-    throws IOException {
-    	if (desiredAttributes != null)
-    	{
-    		FileOutputStream fileOut = null;
-    		String fileName;
-    		fileName = Util.joinPaths(searchDir, "attributes_" + attributeCounter);
+            throws IOException {
+        if (desiredAttributes != null) {
+            FileOutputStream fileOut = null;
+            String fileName;
+            fileName = Util.joinPaths(searchDir, "attributes_" + attributeCounter);
 
-    		try {
-    			File f = new File(fileName);
-    			fileOut = new FileOutputStream(f);
-    			for (String s : desiredAttributes) {
-    				if (s != null)
-    					fileOut.write((s + "\n").getBytes());
-    			}
-    		} finally {
-    			try {
-    				fileOut.close();
-    			} catch (IOException ignore) {
-    			}
-    		}
+            try {
+                File f = new File(fileName);
+                fileOut = new FileOutputStream(f);
+                for (String s : desiredAttributes) {
+                    if (s != null)
+                        fileOut.write((s + "\n").getBytes());
+                }
+            } finally {
+                try {
+                    fileOut.close();
+                } catch (IOException ignore) {
+                }
+            }
 
-    		attributeCounter++;
+            attributeCounter++;
 
-    		javaLogger.log(Level.FINEST, "Saving attributes.", fileName);
-    	}
+            javaLogger.log(Level.FINEST, "Saving attributes.", fileName);
+        }
     }
 
     @Override
     public void saveSessionVariables(Map<String, Double> sessionVariables)
-    throws IOException {
-    	synchronized (lock) {
-    		if (sessionVariables != null) {
-    			FileOutputStream fileOut = null;
-    			String fileName;
-    			fileName = Util.joinPaths(searchDir, "sessionVariables_"
-    					+ sessionCounter);
-    			try {
-    				File f = new File(fileName);
-    				fileOut = new FileOutputStream(f);
-    				for (Map.Entry<String, Double> entry : sessionVariables
-    						.entrySet()) {
-    					if (entry.getValue() != null)
-    						fileOut.write((Double.toString(entry.getValue()) + "\n")
-    							.getBytes());
-    				}
-    			} finally {
-    				try {
-    					fileOut.close();
-    				} catch (IOException ignore) {
-    				}
-    			}
-    			sessionCounter++;
+            throws IOException {
+        synchronized (lock) {
+            if (sessionVariables != null) {
+                FileOutputStream fileOut = null;
+                String fileName;
+                fileName = Util.joinPaths(searchDir, "sessionVariables_"
+                        + sessionCounter);
+                try {
+                    File f = new File(fileName);
+                    fileOut = new FileOutputStream(f);
+                    for (Map.Entry<String, Double> entry : sessionVariables
+                            .entrySet()) {
+                        if (entry.getValue() != null)
+                            fileOut.write((Double.toString(entry.getValue()) + "\n")
+                                    .getBytes());
+                    }
+                } finally {
+                    try {
+                        fileOut.close();
+                    } catch (IOException ignore) {
+                    }
+                }
+                sessionCounter++;
 
-    			javaLogger.log(Level.FINEST, "Saving Session Variables.", fileName);
-    		}
-    	}
+                javaLogger.log(Level.FINEST, "Saving Session Variables.", fileName);
+            }
+        }
     }
 
     @Override
     public void stoppedSearch(Throwable cause) {
         synchronized (lock) {
-            javaLogger.log(Level.FINEST, "Search has stopped.", new String[] {
+            javaLogger.log(Level.FINEST, "Search has stopped.", new String[]{
                     Long.toString(totalObjects),
                     Long.toString(processedObjects),
                     Long.toString(droppedObjects),
                     Long.toString(truepositiveObjects),
-                    Long.toString(falsenegativeObjects) });
+                    Long.toString(falsenegativeObjects)});
             shutdown(cause);
         }
     }
@@ -336,61 +312,61 @@ class XMLLogger extends LoggingFramework {
 
     @Override
     public void updateStatistics(Map<String, ServerStatistics> result) {
-    	synchronized (lock) {
-    		if (result != null) {
-    			totalObjects = 0;
-    			processedObjects = 0;
-    			droppedObjects = 0;
-    			truepositiveObjects = 0;
-    			falsenegativeObjects = 0;
-    			for (ServerStatistics ss : result.values()) {
-    				if (ss != null) {
-					Map<String, Long> map = ss.getServerStats();
-					totalObjects += map.get(ss.TOTAL_OBJECTS);
-					processedObjects += map.get(ss.PROCESSED_OBJECTS);
-					droppedObjects += map.get(ss.DROPPED_OBJECTS);
-					truepositiveObjects += map.get(ss.TP_OBJECTS);
-					falsenegativeObjects += map.get(ss.FN_OBJECTS);
-    				}
-    			}
+        synchronized (lock) {
+            if (result != null) {
+                totalObjects = 0;
+                processedObjects = 0;
+                droppedObjects = 0;
+                truepositiveObjects = 0;
+                falsenegativeObjects = 0;
+                for (ServerStatistics ss : result.values()) {
+                    if (ss != null) {
+                        Map<String, Long> map = ss.getServerStats();
+                        totalObjects += map.get(ss.TOTAL_OBJECTS);
+                        processedObjects += map.get(ss.PROCESSED_OBJECTS);
+                        droppedObjects += map.get(ss.DROPPED_OBJECTS);
+                        truepositiveObjects += map.get(ss.TP_OBJECTS);
+                        falsenegativeObjects += map.get(ss.FN_OBJECTS);
+                    }
+                }
 
-    			javaLogger.log(Level.FINEST, "Updating server statistics.",
-					new String[] { Long.toString(totalObjects),
-					Long.toString(processedObjects),
-					Long.toString(droppedObjects),
-					Long.toString(truepositiveObjects),
-					Long.toString(falsenegativeObjects) });
-    		}
-    	}
+                javaLogger.log(Level.FINEST, "Updating server statistics.",
+                        new String[]{Long.toString(totalObjects),
+                                Long.toString(processedObjects),
+                                Long.toString(droppedObjects),
+                                Long.toString(truepositiveObjects),
+                                Long.toString(falsenegativeObjects)});
+            }
+        }
     }
 
     @Override
     public void saveGetNewResult(Result result) {
         synchronized (lock) {
             String array[] = null;
-        	if (result != null) {
-	            if (Boolean
-	                    .getBoolean("edu.cmu.cs.diamond.opendiamond.loggingframework.detailedresults")) {
-	                array = new String[result.getKeys().size() * 2 + 1];
-	                int i = 1;
-	                for (String s : result.getKeys()) {
-	                    if (s != null)
-	                    	array[i] = s;
-	                    if (s != null && result.getValue(s) != null)
-	                    	array[i + 1] = Base64.encodeBytes(result.getValue(s));
-	                    i += 2;
-	                }
-	                if (result.getObjectIdentifier() != null)
-	                	array[0] = result.getObjectIdentifier().getHostname();
-	            } else {
-	                if (result.getObjectIdentifier() != null)
-	                	array = new String[] {
-	                        result.getObjectIdentifier().getHostname(),
-	                        result.toString() };
-	            }
-	
-	            javaLogger.log(Level.FINEST, "Got new result.", array);
-        	}
+            if (result != null) {
+                if (Boolean
+                        .getBoolean("edu.cmu.cs.diamond.opendiamond.loggingframework.detailedresults")) {
+                    array = new String[result.getKeys().size() * 2 + 1];
+                    int i = 1;
+                    for (String s : result.getKeys()) {
+                        if (s != null)
+                            array[i] = s;
+                        if (s != null && result.getValue(s) != null)
+                            array[i + 1] = Base64.encodeBytes(result.getValue(s));
+                        i += 2;
+                    }
+                    if (result.getObjectIdentifier() != null)
+                        array[0] = result.getObjectIdentifier().getHostname();
+                } else {
+                    if (result.getObjectIdentifier() != null)
+                        array = new String[]{
+                                result.getObjectIdentifier().getHostname(),
+                                result.toString()};
+                }
+
+                javaLogger.log(Level.FINEST, "Got new result.", array);
+            }
         }
     }
 
@@ -427,14 +403,14 @@ class XMLLogger extends LoggingFramework {
 
     @Override
     public void saveSearchFactory(SearchFactory searchFactory,
-            Set<String> desiredAttributes) throws IOException {
+                                  Set<String> desiredAttributes) throws IOException {
         synchronized (lock) {
-        	if (searchFactory != null) {
-	            javaLogger.log(Level.FINEST, "Saving search factory.");
-	            saveFilters(searchFactory.getFilters());
-	            saveCookieMap(searchFactory.getCookieMap());
-	            saveAttributes(desiredAttributes);
-        	}
+            if (searchFactory != null) {
+                javaLogger.log(Level.FINEST, "Saving search factory.");
+                saveFilters(searchFactory.getFilters());
+                saveCookieMap(searchFactory.getCookieMap());
+                saveAttributes(desiredAttributes);
+            }
         }
     }
 }
